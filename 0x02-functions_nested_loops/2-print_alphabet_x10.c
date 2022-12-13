e2e85a91ef5b0130e81358241275597f5704e7fe
mitchelhart <mitchelhartr@gmail.com>
2022-12-13 12:47:25 -0800

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,6 +5,7 @@
  *
  * Return: void
  */
+
 void print_alphabet_x10(void)
 {
 	int round = '0';
@@ -12,14 +13,14 @@ void print_alphabet_x10(void)
 
 	while (round < 10)
 	{
-		letter = 'a';
-		while (letter <= 'z')
-		{
-		_putchar(letter);
-		letter++;
-		}
-		_putchar('\n');
+	letter = 'a';
+	while (letter <= 'z')
+	{
+	_putchar(letter);
+	letter++;
+	}
+	_putchar('\n');
 
-		round++;
+	round++;
 	}
 }
