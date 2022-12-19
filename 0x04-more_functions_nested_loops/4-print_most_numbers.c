e09dbc650e46c774fb7853cc6ce59581746099ab
mitchelhart <mitchelhartr@gmail.com>
2022-12-19 07:38:17 -0800

    task 3

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,17 +1,16 @@
 #include "main.h"
 /**
- * print_most_numbers - prints numbe
+ * print_most_numbers - prints number
+ * Return: 0 Always
  */
 void print_most_numbers(void)
 {
-	int i;
+	int i = '0';
 
-	for (i = 48; i < 58; i++)
+	for (; i <= '9'; i++)
 	{
-		if (i != 50 && i != 52)
-		{
+		if (i != '2' && i != '4')
 			_putchar(i);
-		}
 	}
 	_putchar('\n');
 }
